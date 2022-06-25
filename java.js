fa33e5e41e0484671bc0e84b0caaa3a7b678d58c
Testimony <adebayotayotestimony@gmail.com>
2022-06-25 18:22:16 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,14 @@
+let fullName= "Testimony Adebayo-Tayo"
+let height="185cm"
+let country= "Nigeria"
+alert(fullName);
+alert(height);
+alert(country);
+
+document.writeln('My name is ', fullName);
+document.writeln("<br> My height is ", height);
+document.writeln("<br> I am from ", country);
+
+console.log(fullName);
+console.log(height);
+console.log(country);
